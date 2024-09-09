48d4cb139c3a9b1f6236e90b07c2ed46568a2040
pmg000915 <pmg000915@naver.com>
2024-09-09 12:37:09 +0900

    v0.5 BP2

--- week03/week03_pandas.py
+++ week03/week03_pandas.py
@@ -1,10 +1,9 @@
 import pandas as pd
 
-df = pd.DataFrame(
-    [
-        [14,72,100],
-        [51,8,11],
-        [6,72,12]
-    ],
-        index=[1,2,3], columns=["a","b","c"])
+data = [[14, 72, 100], [51, 8, 11], [6, 72, 12]]
+df = pd.DataFrame(data, index=[1, 2, 3], columns=["a", "b", "c"])
 print(df)
+print(df.shape, df.dtypes)
+print(df['b'].value_counts())
+print(df['b'].nunique())
+print(df.describe())

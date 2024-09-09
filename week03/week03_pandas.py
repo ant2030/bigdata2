055873009008642cf6d73acae5efddeda7c1d102
pmg000915 <pmg000915@naver.com>
2024-09-09 12:27:17 +0900

    v0.4 BP2

--- week03/week03_pandas.py
+++ week03/week03_pandas.py
@@ -1,13 +1,5 @@
 import pandas as pd
 
-# df = pd.DataFrame(
-#     {
-#         "a": [14,51,6],
-#         "b": [72,8,72],
-#         "c": [100,11,12]
-#
-#     },index=[1,2,3]
-# )
 df = pd.DataFrame(
     [
         [14,72,100],

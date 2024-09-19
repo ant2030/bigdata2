2f33e6fdda8867b2c9f852678961b2fe70fa750c
pmg000915 <pmg000915@naver.com>
2024-09-19 10:25:09 +0900

    0.8v

--- week04/week04_web01.py
+++ week04/week04_web01.py
@@ -5,7 +5,6 @@ api='https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp'
 
 station_id=input("지역코드 : ") #전국 108 ,수도권 109, 강원 105, 제주 184...
 values={'stnId:':station_id}
-
-url = api + '?' + str(values)
-
+url = api + '?' + urllib.parse.urlencode(values)
 print(url)
+

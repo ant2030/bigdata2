492c362dd6bbc745dbd7cc93c67a9e36f2398766
pmg000915 <pmg000915@naver.com>
2024-10-31 12:37:16 +0900

    v1.7

--- week10/week10_dataset.py
+++ week10/week10_dataset.py
@@ -1,8 +1,19 @@
 import seaborn as sns
 import pandas as pd
 
-print(sns.get_dataset_names())
-mpg= sns.load_dataset('mpg')
-print(mpg.head().sort_values('mpg'))
-
-
+# print(sns.get_dataset_names())
+# mpg= sns.load_dataset('mpg')
+# print(mpg.head().sort_values('mpg'))
+#
+#
+titanic = sns.load_dataset(('titanic'))
+# print(titanic.describe())
+# print(titanic.head())
+# print(len(titanic))
+# print(titanic.tail(10))
+# print(titanic.info())
+# print(titanic['deck'])
+titanic['deck'] = titanic ['deck'].cat.add_categories('unknown')
+# print(titanic['deck'])
+titanic['deck'].fillna('unknown',inplace=True)
+print(titanic.info())
